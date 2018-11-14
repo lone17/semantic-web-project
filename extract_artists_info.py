97c1ff267fc9f31838fe99faced46c1ce9555868
lone17 <toilamduoctatca@gmail.com>
2018-11-14 13:39:39 +0700

    update artist kb: pred 1 (birth_name), 3 (dob) and artist's country

--- extract_artists_info.py
+++ extract_artists_info.py
@@ -1,17 +1,20 @@
 from utils import *
 import pickle
+import traceback
 
 sep = '##### NEW FILE #####\n'
 error = []
-site = 'zing'
+site = 'nct'
 
 if site == 'zing':
     files = ['zing/tieu_su_' + str(i) + '.txt' for i in range(3)]
 elif site == 'nct':
     files = ['nct/tieu_su_' + str(i) + '.txt' for i in range(1)]
 
-with open('tmp' + '.kb', 'rb') as f:
+with open(site + '_artists.kb', 'rb') as f:
+# with open('tmp.kb', 'rb') as f:
     kb = pickle.load(f)
+error = []
 
 for file in files:
     print('>>>', file)
@@ -21,29 +24,47 @@ for file in files:
     for tieu_su in list_tieu_su:
         url, html = tieu_su.split('\n', 1)
 
-        if url not in kb:
+        if url not in kb or error_404 in html:
             continue
 
         try:
             name = kb[url]['name']
-            professions = kb[url]['professions']
-            instruments = kb[url]['instruments']
+            # professions = kb[url]['professions']
+            # instruments = kb[url]['instruments']
             # name = extract_artist_name(html, site)
             # text = extract_biography(html, site)
             # professions = extract_professions(text)
             # instruments = extract_instruments(text)
 
+            info = extract_artist_info(html, site)
+            kb[url]['dob'] = info['dob']
+            if ' == ' in name:
+                kb[url]['birth_name'] = name.split(' == ')[1]
+            else:
+                kb[url]['birth_name'] = info['birth_name']
+            if 'dob' not in kb[url]:
+                kb[url]['dob'] = info['dob']
+            if ' >> ' in name:
+                kb[url]['country'] = name.split(' >> ')[1]
+            else:
+                kb[url]['country'] = info['country']
+
             # kb[url]['name'] = name
             # kb[url]['professions'].update(professions)
             # kb[url]['instruments'].update(instruments)
 
+            print('*' * 80)
             print('url:', url)
             print('name:', name)
-            print('professions:', professions)
-            print('instruments:', instruments)
+            # print('professions:', professions)
+            # print('instruments:', instruments)
+            print(kb[url])
             print()
         except:
             error.append(url)
 
-# with open(site + '.kb', 'wb') as f:
+# with open(site + '_artists.kb', 'wb') as f:
 #     pickle.dump(kb, f)
+#
+# with open('error.pickle', 'wb') as f:
+#     pickle.dump(error, f)
--- extract_songs_info.py
+++ extract_songs_info.py
@@ -2,16 +2,18 @@ from utils import *
 import pickle
 import traceback
 import os
+from datetime import datetime
 
 sep = '##### NEW FILE #####\n'
-site = 'nct'
+site = 'zing'
 
 if site == 'zing':
     files = ['zing/songs_' + str(i) + '.txt' for i in range(52)]
 elif site == 'nct':
     files = ['nct/songs_' + str(i) + '.txt' for i in range(15)]
 
-kb_file = site + '_songs.kb'
+# kb_file = site + '_songs.kb'
+kb_file = 'tmp.kb'
 if os.path.exists(kb_file):
     with open(kb_file, 'rb') as f:
         kb = pickle.load(f)
@@ -22,9 +24,10 @@ if os.path.exists('error.pickle'):
     with open('error.pickle', 'rb') as f:
         error = pickle.load(f)
 else:
-    error = {}
+    error = []
 
 for file in files:
+    print(datetime.now())
     print('>>>', file)
     with open(file, 'r', encoding='utf8') as f:
         pages = f.read().split(sep)[1:]
@@ -32,8 +35,10 @@ for file in files:
     for page in pages:
         url, html = page.split('\n', 1)
 
-        if url not in kb:
+        if error_404 in html:
             continue
+
+        kb[url] = {}
 
         try:
             name = extract_song_name(html, site)
@@ -46,19 +51,19 @@ for file in files:
             kb[url]['composed_by'] = composers
             kb[url]['lyric'] = lyric
 
-            print('#'*80)
-            print('url:', url)
-            print('name:', name)
-            print('performers:', performers)
-            print('composers:', composers)
-            print('lyric:')
-            print(lyric[:100])
-            print()
+            # print('#'*80)
+            # print('url:', url)
+            # print('name:', name)
+            # print('performers:', performers)
+            # print('composers:', composers)
+            # print('lyric:')
+            # print(lyric[:100])
+            # print()
         except Exception as e:
-            error[url] = e.__traceback__
+            error.append(url)
 
-with open(kb_file, 'wb') as f:
-    pickle.dump(kb, f)
+    with open(kb_file, 'wb') as f:
+        pickle.dump(kb, f)
 
-with open('error.pickle', 'wb') as f:
-    pickle.dump(error, f)
+    with open('error.pickle', 'wb') as f:
+        pickle.dump(error, f)
